6473cde1814b1f7c54857f7785679ee3a8992f89
oliviervdt <oliviervdt@06a30580-f254-11dd-89c5-9df526a60542>
2009-03-02 22:51:31 +0000

    Deuxieme version (Olivier)

--- trunk/TP2/aurelus/TP2_evol.java
+++ trunk/TP2/aurelus/TP2_evol.java
@@ -12,81 +12,109 @@
  
 public class TP2_evol {
 
-        static char [] tab = new char 10?;
-
-    // Autres méthodes s'il y a lieu
+        static char [] tab;
+       
     public static int saisir (char[] tab2){
         String chaineS;
         int nbElem = 0;
         int longueur = 0;
-//         char [] tab;
-       
+      
         System.out.println( "Donnez une chaîne: " );
         chaineS = Clavier.lireString();
-       
-        longueur = chaineS.length() - 1;
-        tab = new char longueur + 1?;
-
-        for ( int i = 0; i <= longueur; i++ ){
-            if ( chaineS.charAt( i ) != 0) {
-                 tabi? = epurer( chaineS.charAt( i ) );
-                nbElem ++;
+        longueur = chaineS.length();
+        tab = enleverEspaces(longueur, chaineS.toCharArray());
+        
+        longueur = tab.length;
+        
+        for ( int i = 0; i < longueur; i++ ){
+            tab[i] = epurer(tab[i]);
+            nbElem ++;
             }
-        }
+        
+        System.out.println("saisir: " + nbElem);
         return nbElem;
     }
-   
+    // Methode pour enlever les espaces et les caracteres de ponctuation
+    public static char[] enleverEspaces(int n, char[] tab) {
+    	int[] notValidChars = { 39, 32, 33, 34, 39, 44, 46, 58, 59, 63 };
+    	final int nb = 10;	// Il y aura toujours 10 caracteres "invalides" (espaces, signes de pontuation);
+    	char[] newTab = new char[n];
+    	char[] finalTab;
+    	boolean found;
+    	int count = 0;
+    	
+    	for (int i = 0; i < n;i++) {
+    		found = false;    		
+    		for (int j = 0; j < nb && !found; j++) {
+    			if ((int)tab[i] == notValidChars[j])
+    				found = true;
+    		}
+    		if (!found) {
+    			newTab[count] = tab[i];
+    			count++;
+    		}
+    	}
+    	
+    	finalTab = new char[count];
+    
+    	for (int i = 0; i < count; i++)
+    		finalTab[i] = newTab[i];
+    	
+    	return finalTab;
+    }
+   // Methode pour enlever les accents et les majuscules. (caractere par caractere)
     public static  char epurer ( int c ){        
        
-            if (( c >= 65 && c <= 90 ) || ( c >= 192  &&  c <= 223 )) {
+            if (( c >= 65 && c <= 90 ) || ( c >= 192  &&  c <= 223 )) { // Conversion a minuscule
                 c += 32;
-//             } else if ( c < 48 || c > 57 ){
-//                 c = 0;
             }
            
-            if ( c >= 224 &&  c  <= 229 ){
+            if ( c >= 224 && c <= 229 ){ // a + accent
                 c = 97;
-            } else if ( c >= 232 && c <= 235 ){
+            } else if ( c >= 232 && c <= 235 ){ // e + accent 
                 c = 101;
-            } else if ( c >= 236 && c <= 239 ){
+            } else if ( c >= 236 && c <= 239 ){ // i + accent
                 c = 105;
-            } else if ( c >= 242 && c <= 246 ){
+            } else if ( c >= 242 && c <= 246 ){ // o + accent
                 c = 111;
-            } else if ( c >= 249 && c <= 252 ){
+            } else if ( c >= 249 && c <= 252 ){ // u + accent
                 c = 117;
-            } else if ( c 231 ){
+            } else if ( c == 231 ){ // c + accent
                 c = 99;
-            } else if ( c 241 ){
+            } else if ( c == 241 ){ // n + accent
                 c = 110;
-            } else if ( c == 255 ){
+            } else if ( c == 255 ){ // y + accent
                 c = 121;
             }
             return (char)c;
         }
      
-           
+    
+    // Methode qui verifie si la chaine est un palindrome
     public static boolean palin ( int n, char [] tab){
         boolean egal = true;
-       
-        for( int i = 0; i <= n && egal; i++ ){
-           
-            if ( tabi? != tabn - i? ){
-                egal = false;
+        System.out.println("palin: " + n);
+        for(int i = 0; i <= n && egal; i++, n--){ // Analyse du tableau de caracteres
+        	System.out.println("n =" + (n - 1) + " i =" + i);
+        	System.out.println("n =" +tab[n - 1] + " i =" + tab[i]);
+           if (tab[i] != tab[n - 1]) {
+               egal = false;
             }
         }
         return egal;
     }
-
+    
     public static void main (String[] params) {
        
-        int n = 1;
+        int n = 0;
         String chaineS = "";
         String chaineT = "";
        
-        n = saisir(tab) - 1; 
-
+        n = saisir(tab); 
+        System.out.println("Apres saisir:" + n);
         while ( n >= 0 ){
-               
+         	System.out.println(tab);
+         	System.out.println("main: " + n);
             if ( palin ( n, tab)){
                 System.out.println( "Ceci est un palindrome." );
             } else {
@@ -99,3 +127,4 @@ public class TP2_evol {
     } // main
    
 } // TP2_evol
+
